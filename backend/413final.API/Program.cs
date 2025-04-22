acf02933684c7eba8500cfeb8d10b282f86086a3
Zac Oldroyd <137462136+zoldroyd@users.noreply.github.com>
2025-04-21 21:00:47 -0600

    initial commit

---
+++ backend/413final.API/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using WaterProject.API.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<WaterDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("WaterConnection")));
+
+builder.Services.AddCors(options =>
+    options.AddPolicy("AllowReactAppBlah",
+        policy => {
+            policy.WithOrigins("http://localhost:3000", "https://black-rock-02665271e.6.azurestaticapps.net")
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+    }));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowReactAppBlah");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ frontend/src/types/Project.ts
@@ -0,0 +1,9 @@
+export interface Project {
+  projectId: number;
+  projectName: string;
+  projectType: string;
+  projectRegionalProgram: string;
+  projectImpact: number;
+  projectPhase: string;
+  projectFunctionalityStatus: string;
+}
